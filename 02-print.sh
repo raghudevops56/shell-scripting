6d3cf78d1b9394f16d902eae6a5014abea29cc52
KalluruS <>
2021-05-27 06:56:48 +0530

    Update shell-scripting/

---
+++ 02-print.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# echo and printf commands can be used to print some message on the screen
+# However we use echo command here , because it is simple and yet effective to all the needs
+
+# Syntax: echo Input Message
+
+echo Hello World
