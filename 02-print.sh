7de503b099f163dc58ed26f3caeba89f5d798ad2
KalluruS <>
2021-05-27 07:05:45 +0530

    Update shell-scripting/

--- 02-print.sh
+++ 02-print.sh
@@ -13,10 +13,24 @@ echo Hello World
 echo -e "Hello,\nWelcome to DevOps training"
 echo -e "\nHello,\n\nWelcome to DevOps training"
 
-## \t is to print a tab space 
+## Print a paragraph
+## \t is to print a tab space
 echo -e "\tThe storm, which had weakened during the afternoon,
         had left the coastal states on edge, with the Bengal
         government claiming that at least one crore people have
         been affected by the calamity thus far, as it hollered
         on its destructive path towards Jharkhand."
 
+
+## Print Text in Colors
+# syntax: echo -e "\e[COLORCODEmMessage"
+
+# COLORS        CODE
+# RED           31
+# GREEN         32
+# YELLOW        33
+# BLUE          34
+# MAGENTA       35
+# CYAN          36
+
+echo -e "\e[31mHello In Red Color"
