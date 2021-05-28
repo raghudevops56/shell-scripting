491830d644a6b72e0eec9e9aabfb8d5391c306e1
KalluruS <>
2021-05-28 06:27:06 +0530

    Update shell-scripting/

--- 03-vars.sh
+++ 03-vars.sh
@@ -9,3 +9,8 @@ echo Course Name = $COURSE
 #DATE=2021-05-28
 DATE=$(date +%F)
 echo Good Morning, Today date is $DATE
+
+NO_OF_USERS=$(who | wc -l)
+echo Number of Users Logged IN in System = $NO_OF_USERS
+
+
