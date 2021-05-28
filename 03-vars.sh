a40467baf857d7e5cdff003fc549c34c6145b4f9
KalluruS <>
2021-05-28 06:29:28 +0530

    Update shell-scripting/

--- 03-vars.sh
+++ 03-vars.sh
@@ -13,4 +13,7 @@ echo Good Morning, Today date is $DATE
 NO_OF_USERS=$(who | wc -l)
 echo Number of Users Logged IN in System = $NO_OF_USERS
 
+ADD=$((2+3+4))
+BIG=$((1+2-3*4/5-7/8%9))
+echo BIG = $BIG
 
