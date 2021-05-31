3f13a1e1924a09b164b494cf6b9da0251199a202
KalluruS <>
2021-05-31 06:48:18 +0530

    Update shell-scripting/

--- 07-functions.sh
+++ 07-functions.sh
@@ -6,10 +6,10 @@ SAMPLE() {
   echo Welcome to SAMPLE Function
   echo Value of a = $a
   b=20
-  echo First Argument = $1 
+  echo First Argument = $1
 }
 
 ## Access your function
 a=10
-SAMPLE
+SAMPLE xyz 
 echo Value of b = $b
