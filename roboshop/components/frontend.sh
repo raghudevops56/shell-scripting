864562702a297ffe387b64fab95aff96cbdadc6f
KalluruS <>
2021-06-01 07:17:10 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -21,7 +21,7 @@ rm -rf /usr/share/nginx/html/*
 STAT $?
 
 HEAD "Extract Downloaded Content"
-unzip -d /usr/share/nginx/html /tmp/frontend.zip
+unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
 STAT $?
 
 # mv frontend-main/* .
