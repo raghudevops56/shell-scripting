95acf7a8d605aa234ae203bf7001cce0b53aafb6
KalluruS <>
2021-06-01 06:43:56 +0530

    Update shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -2,4 +2,11 @@ HEAD() {
   echo -n -e "\e[1m $1 \e[0m \t\t ... "
 }
 
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32m done\e[0m"
+  else
+    echo -e "\e[1;31m fail\e[0m"
+  fi
+}
 
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,4 +4,4 @@ source components/common.sh
 
 HEAD "Installing Nginx"
 yum install nginx -y &>>/tmp/roboshop.log
-echo -e "\e[32mdone\e[0m"
+STAT $?
