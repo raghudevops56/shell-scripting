b56401c1f90fecb6027bfc59ddd379f0fa0e233a
KalluruS <>
2021-06-01 07:11:36 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,3 +6,9 @@ rm -f /tmp/roboshop.log
 HEAD "Installing Nginx"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
+
+HEAD "Start Nginx"
+systemctl start nginx &>>/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
+STAT $?
+
