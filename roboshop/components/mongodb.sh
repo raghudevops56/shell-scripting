2075ff217a73cb2209d1e02079b1d7a11e071ecf
KalluruS <>
2021-06-01 07:25:47 +0530

    Update shell-scripting/

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -12,7 +12,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 STAT $?
 
-HEAD "Install MongoDB\t\t"
+HEAD "Install MongoDB\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
 STAT $?
 
