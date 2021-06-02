e8d8484dc72fa507fbbf76fe4a494c8f1a8b4b23
KalluruS <>
2021-06-02 06:35:59 +0530

    Update shell-scripting/

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -11,4 +11,4 @@ fi
 LID=lt-090f6832097248547
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
