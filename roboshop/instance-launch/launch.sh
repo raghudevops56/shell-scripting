5eb0caed733a1b7c72c1a1f6d51303958fb5f1af
KalluruS <>
2021-06-02 06:34:44 +0530

    Update shell-scripting/

---
+++ roboshop/instance-launch/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COMPONENT=$1
+
+## -z validates the variable empty , true if it is empty.
+if [ -z "${COMPONENT}" ]; then
+  echo "Component Input is Needed"
+  exit 1
+fi
+
+LID=lt-090f6832097248547
+LVER=1
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
