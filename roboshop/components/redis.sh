a8f06f50e1b6fe2349cd1896a1daf18c0800663b
KalluruS <>
2021-06-03 07:15:44 +0530

    Update shell-scripting/

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo Installing Redis
+source components/common.sh
+rm -f /tmp/roboshop.log
+
+HEAD "Setup Redis Repos"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/roboshop.log && yum-config-manager --enable remi &>>/tmp/roboshop.log
+STAT $?
+
+
+# yum install redis -y
