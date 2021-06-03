19417091b055ccd576b42f62f867bad58460d64c
KalluruS <>
2021-06-03 06:24:07 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -8,8 +8,14 @@ yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
 STAT $?
 
 HEAD "Add RoboShop App User"
-useradd roboshop &>>/tmp/roboshop.log
-STAT $?
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo User is already there, So Skipping the User creation &>>/tmp/roboshop.log
+  STAT $?
+else
+  useradd roboshop &>>/tmp/roboshop.log
+  STAT $?
+fi 
 
 HEAD "Download App from GitHub"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
