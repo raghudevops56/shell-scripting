b15bf97d2e98f10bcd37695bd94d7639cc5d1eaa
KalluruS <>
2021-06-03 06:14:28 +0530

    Update shell-scripting/

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo Installing Catalogue
+source components/common.sh
+rm -f /tmp/roboshop.log
+
+HEAD "Install NodeJS"
+yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Add RoboShop App User"
+useradd roboshop &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Download App from GitHub"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract the Downloaded Archive"
+cd /home/roboshop && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+STAT $?
+
+HEAD "Install NodeJS Dependencies"
+cd /home/roboshop/catalogue && npm install &>>/tmp/roboshop.log
+STAT $?
+
+
+
+
