44ad79e86d7c8f47b94f8df0eda29d127a32295d
KalluruS <>
2021-06-04 06:32:42 +0530

    Update shell-scripting/

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo Installing MySQL
+source components/common.sh
+rm -f /tmp/roboshop.log
+set-hostname mysql
+
+HEAD "Setup MySQL Repo"
+echo '[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+STAT $?
+
+HEAD "Install MySQL Server"
+yum remove mariadb-libs -y &>>/tmp/roboshop.log && yum install mysql-community-server -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Start MySQL Service"
+systemctl enable mysqld &>>/tmp/roboshop.log && systemctl start mysqld &>>/tmp/roboshop.log
+STAT $?
+
+
