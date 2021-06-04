9fed0657589ee7837650f2437dfbe08ceb8a78c0
KalluruS <>
2021-06-04 07:05:40 +0530

    Update shell-scripting/

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo Installing RabbitMQ
+source components/common.sh
+rm -f /tmp/roboshop.log
+set-hostname rabbitmq
+
+HEAD "Install ERLANG"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Setup RabbitMQ Yum Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Install RabbitMQ Server"
+yum install rabbitmq-server -y  &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Start RabbitMQ Server"
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log && systemctl start rabbitmq-server &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Create Application User in RabbitMQ"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log && rabbitmqctl set_user_tags roboshop administrator &>>/tmp/roboshop.log && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
+STAT $?
+
+
