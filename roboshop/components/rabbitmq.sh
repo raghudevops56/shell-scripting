9b6e42004b53ecaa31fec51c634c10896ac82c60
KalluruS <>
2021-06-04 07:18:30 +0530

    Update shell-scripting/

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -5,9 +5,15 @@ rm -f /tmp/roboshop.log
 set-hostname rabbitmq
 
 HEAD "Install ERLANG"
-yum list installed | grep erlang
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>/tmp/roboshop.log
-STAT $?
+yum list installed | grep erlang &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo "Erlang already Installed" &>>/tmp/roboshop.log
+  STAT $?
+else
+  yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>/tmp/roboshop.log
+  STAT $?
+fi
+
 
 HEAD "Setup RabbitMQ Yum Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>/tmp/roboshop.log
