50ae8abdb705c2d4091795ec7f18ec1926555d81
KalluruS <>
2021-06-04 06:15:41 +0530

    Update shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -12,3 +12,46 @@ STAT() {
   fi
 }
 
+APP_USER_ADD() {
+  HEAD "Add RoboShop App User\t\t"
+  id roboshop &>>/tmp/roboshop.log
+  if [ $? -eq 0 ]; then
+    echo User is already there, So Skipping the User creation &>>/tmp/roboshop.log
+    STAT $?
+  else
+    useradd roboshop &>>/tmp/roboshop.log
+    STAT $?
+  fi
+}
+
+NODEJS() {
+  HEAD "Install NodeJS\t\t\t"
+  yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+  STAT $?
+
+  APP_USER_ADD
+
+  HEAD "Download App from GitHub\t"
+  curl -s -L -o /tmp/$1.zip "https://github.com/roboshop-devops-project/$1/archive/main.zip" &>>/tmp/roboshop.log
+  STAT $?
+
+  HEAD "Extract the Downloaded Archive"
+  cd /home/roboshop && rm -rf $1 && unzip /tmp/$1.zip &>>/tmp/roboshop.log && mv $1-main $1
+  STAT $?
+
+  HEAD "Install NodeJS Dependencies\t"
+  cd /home/roboshop/$1 && npm install --unsafe-perm &>>/tmp/roboshop.log
+  STAT $?
+
+  HEAD "Fix Permissions to App Content"
+  chown roboshop:roboshop /home/roboshop -R
+  STAT $?
+
+  HEAD "Setup SystemD Service\t\t"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/$1/systemd.service  && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
+  STAT $?
+
+  HEAD "Start $1 Service\t"
+  systemctl daemon-reload && systemctl enable $1 &>>/tmp/roboshop.log && systemctl restart $1 &>>/tmp/roboshop.log
+  STAT $?
+}
--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo Installing User
+source components/common.sh
+rm -f /tmp/roboshop.log
+set-hostname user
+
+NODEJS "user"
