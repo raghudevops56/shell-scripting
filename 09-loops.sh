9e920511460015728c5d44c4a433d5449925b238
KalluruS <>
2021-06-05 06:19:55 +0530

    Update shell-scripting/

---
+++ 09-loops.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fundamentally there are two loops 1. while 2. for
+# Inverse the logic of those two loops we have another tow more loops 3. until 4. select
+
+# Syntax
+# while [ expression ] ; do
+# commands
+#done
+
+i=10
+while [ $i -gt 0 ]; do
+  echo $i
+  i=$(($i-1))
+done
+
